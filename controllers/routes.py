bf218f764826378505de0c7970ca65596264549b
GabrielKenzoT <gabrieltakeuchi418@gmail.com>
2025-04-17 23:56:13 -0300

    ta quase terminando

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template, request
 
 filmelist = [
-    {'titulo': 'Lego Batman','ano': 2018,'genero': 'Animação'},
-    {'titulo': 'Uma aventura no Tempo','ano': 2020,'genero': 'Infantil'}
+    {'titulo': 'Lego Batman','ano': 2018,'genero': 'Animação','descricao':'O filme "LEGO Batman: O Filme" é uma comédia animada cheia de ação, que explora o mundo do super-herói solitário enquanto ele aprende o valor da amizade e da colaboração para salvar Gotham City. Com humor cativante e cenas incríveis, é diversão garantida para fãs de todas as idades!'},
+    {'titulo': 'Uma aventura no Tempo','ano': 2020,'genero': 'Infantil', 'descricao': '"Uma Aventura no Tempo" narra a emocionante jornada de jovens exploradores que, ao ativarem um misterioso artefato, são transportados para diferentes períodos históricos. Enfrentando desafios e aprendendo lições valiosas, eles descobrem que o verdadeiro poder do tempo está nas conexões humanas e nas escolhas que moldam o futuro. Uma história envolvente cheia de descobertas e emoção!'}
     ]
+feedbacklist = [
+    {'nome': 'Ramon Trigon','email': 'ramontrigon@hotmail', 'opcoes': 'Outros', 'descricao': 'Cara eu nunca vi um site tão bom como esse, acho que deveria merecer um MB'}
+]
 
 def init_app(app):
     
@@ -12,24 +15,45 @@ def init_app(app):
     def home():
         return render_template('home.html')
     
-    @app.route('/lista')
-    def lista():
-        return render_template('lista.html')
     
     @app.route('/lista', methods=['GET', 'POST'])
+
     def lista():
-        # acessando o primeiro jogo da lista de jogos
-        filmelist = filmelist[0]
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('genero') and request.form.get('descricao'):
+                filmelist.append({'titulo': request.form.get('titulo'), 
+                                'ano': request.form.get('ano'),
+                                'genero': request.form.get('genero'),
+                                'descricao': request.form.get('descricao')})
+                
+        return render_template('lista.html', 
+                               filmelist = filmelist)
    
     
-    @app.route('/feedback')
-    
+    @app.route('/feedback', methods=['GET', 'POST'])
     
     def feedback():
-        return render_template('feedback.html')
-    
-    @app.route('/cadastro')
+        if request.method == 'POST':
+            if request.form.get('nome') and request.form.get('email') and request.form.get('opcoes') and request.form.get('descricao'):
+                feedbacklist.append({'nome': request.form.get('nome'),
+                                'email': request.form.get('email'), 
+                                'opcoes': request.form.get('opcoes'),
+                                'descricao': request.form.get('descricao')})
+                
 
+        return render_template('feedback.html', 
+                               feedbacklist = feedbacklist)
+    
+    @app.route('/cadastro', methods=['GET', 'POST'])
 
     def cadastro():
-        return render_template('cadastro.html')
+        if request.method == 'POST':
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('genero') and request.form.get('descricao'):
+                filmelist.append({'titulo': request.form.get('titulo'), 
+                                'ano': request.form.get('ano'),
+                                'genero': request.form.get('genero'),
+                                'descricao': request.form.get('descricao')})
+                
+        
+        return render_template('cadastro.html',
+                               filmelist = filmelist)
